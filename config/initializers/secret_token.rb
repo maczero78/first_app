684467564c4238a226f2eee7c016d9d169a16592
U-Gate\maczero <maczero@Gate.(none)>
2011-11-06 01:08:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a103cb26c54a90ebaa7f9c64c276b0b6d0982b17500e4c39297c663f5bd9eda432abd127459965a350afcd18d288f11c7b2cfa89e1029ce75de75e45ae2c6753'
